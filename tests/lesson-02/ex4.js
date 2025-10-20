2e0afcb4e4f492a413d02c82138fc45d6c2dc238
HuongPhan <jayphanvn@gmail.com>
2025-10-20 12:51:14 +0700

    chore: add comment for lesson 2 ex4 javascript exercises

--- tests/lesson-02/ex4.js
+++ tests/lesson-02/ex4.js
@@ -1,4 +1,4 @@
-let myTaller = 159;
+let myTaller = 159; //expected input in cm is in between 100 and 200 (100< myTaller <200)
 
 idealTaller = ((myTaller - 100) * 9) / 10;
 maximumTaller = myTaller - 100;
