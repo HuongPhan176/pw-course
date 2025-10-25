db6eca1cda36ec19683f6393281488bf9fecbdf9
HuongPhan <jayphanvn@gmail.com>
2025-10-25 20:45:23 +0700

    fix: lesson 3 - address mentor's comment for homework exercises

--- tests/lesson-03/05-loop.js
+++ tests/lesson-03/05-loop.js
@@ -7,7 +7,7 @@ console.log(sum);
 
 // Loop - task 2
 for (let i = 2; i <= 9; i++) {
-    for (let j = 1; j <= 9; j++) {
+    for (let j = 1; j <= 10; j++) {
         console.log(`${i} x ${j} = ${i * j}`);
     }
 }
